cf7054c23ae0b708aa57ba0ada26d846cccd79ab
alokjoshi <ajoshi@flash.net>
2021-05-24 06:27:22 -0700

    Fixed database error

--- services/dbservice.js
+++ services/dbservice.js
@@ -14,7 +14,8 @@ let knex = require('knex')({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    //ssl:true
+    port: process.env.DB_PORT,
+    ssl:{rejectUnauthorized:false}
   },
   //debug:true
 });
