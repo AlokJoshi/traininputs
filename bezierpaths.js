2d1f461f14712e50ef86e86a0078083a522d3018
alokjoshi <ajoshi@flash.net>
2021-05-25 05:46:09 -0700

    Fixed environment variables

--- bezierpaths.js
+++ bezierpaths.js
@@ -6,8 +6,8 @@ class BezierPaths {
     this.strokestyle=strokestyle
   }
   add(x1, y1, x2, y2, fromx, fromy, tox, toy, speed, roadtype) {
-    let strokestyle = roadtype==Game.HIGHWAY?'rgb(112,112,112)':'brown'
-    let roadwidth = roadtype==Game.HIGHWAY?5:2
+    let strokestyle = roadtype==Game.HIGHWAY?'rgb(224,224,224)':'brown'
+    let roadwidth = roadtype==Game.HIGHWAY?2:1
     let bp = new BezierPath(x1, y1, x2, y2, fromx, fromy, tox, toy, this.ctx_background, this.ctx_foreground,speed,strokestyle,roadwidth)
     this.paths.push(bp)
     return bp
