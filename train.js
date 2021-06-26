50e4f98fc3e0e6f6ca45e59dd3263a479095c437
alokjoshi <ajoshi@flash.net>
2021-06-26 08:38:44 -0700

    milestone for wagons

--- train.js
+++ train.js
@@ -1,43 +1,50 @@
 class Train {
-  constructor(path,passengercoaches,goodscoaches) {
+  constructor(path, passengercoaches, goodscoaches) {
     this.path = path
     this.passengercoaches = passengercoaches
     this.goodscoaches = goodscoaches
     this.passengers = {}
     this.goods = {}
   }
-  addPassengerCoach = async() => {
-    this.passengercoaches++  
+  addPassengerCoach = async () => {
+    this.passengercoaches++
     this.path.game.milestone.send(`Train:${this.path.name} passenger coaches increased to ${this.passengercoaches}`)
-    await saveUpdatedTrainInfoToDB(this.path.PathIdInDB,this.passengercoaches,this.goodscoaches)
+    await saveUpdatedTrainInfoToDB(this.path.PathIdInDB, this.passengercoaches, this.goodscoaches)
   }
-  removePassengerCoach = async() => {
-    this.passengercoaches--  
-    await saveUpdatedTrainInfoToDB(this.path.PathIdInDB,this.passengercoaches,this.goodscoaches)
+  removePassengerCoach = async () => {
+    if (this.passengercoaches > 0) {
+      this.passengercoaches--
+      this.path.game.milestone.send(`Train:${this.path.name} passenger coaches decreased to ${this.passengercoaches}`)
+      await saveUpdatedTrainInfoToDB(this.path.PathIdInDB, this.passengercoaches, this.goodscoaches)
+    }
   }
-  addGoodsCoach = async() => {
-    this.goodscoaches++  
-    await saveUpdatedTrainInfoToDB(this.path.PathIdInDB,this.passengercoaches,this.goodscoaches)
+  addGoodsCoach = async () => {
+    this.goodscoaches++
+    this.path.game.milestone.send(`Train:${this.path.name} wagons increased to ${this.goodscoaches}`)
+    await saveUpdatedTrainInfoToDB(this.path.PathIdInDB, this.passengercoaches, this.goodscoaches)
   }
-  removeGoodsCoach = async() => {
-    this.goodscoaches-- 
-    await saveUpdatedTrainInfoToDB(this.path.PathIdInDB,this.passengercoaches,this.goodscoaches)
+  removeGoodsCoach = async () => {
+    if (this.goodscoaches > 0) {
+      this.goodscoaches--
+      this.path.game.milestone.send(`Train:${this.path.name} wagons decreased to ${this.goodscoaches}`)
+      await saveUpdatedTrainInfoToDB(this.path.PathIdInDB, this.passengercoaches, this.goodscoaches)
+    }
   }
-  get passenger_capacity(){
-    return this.passengercoaches*Game.PASSENGER_COACH_CAPACITY
+  get passenger_capacity() {
+    return this.passengercoaches * Game.PASSENGER_COACH_CAPACITY
   }
-  get wagon_capacity(){
-    return this.goodscoaches*Game.WAGON_CAPACITY
+  get wagon_capacity() {
+    return this.goodscoaches * Game.WAGON_CAPACITY
   }
-  get num_passengers_on_train(){
+  get num_passengers_on_train() {
     //sums up passengers for each city
-    let n=0
+    let n = 0
     for (const city in this.passengers) {
-      n+=this.passengers[city]
+      n += this.passengers[city]
     }
     return n
   }
-  get goods_on_train(){
+  get goods_on_train() {
     //sums up goods for each city
     // let n=0
     // for (const city in this.goods) {
@@ -46,37 +53,37 @@ class Train {
     // return n
     this.wagon_capacity
   }
-  get passenger_room_available(){
-    return this.passenger_capacity-this.num_passengers_on_train
+  get passenger_room_available() {
+    return this.passenger_capacity - this.num_passengers_on_train
   }
-  get wagon_capacity_available(){
-    return this.wagon_capacity-this.goods_on_train
+  get wagon_capacity_available() {
+    return this.wagon_capacity - this.goods_on_train
   }
-  boardPassengersFor(city,num){
+  boardPassengersFor(city, num) {
     //passengers object has a key for each city. The value is the number of passengers
     //travelling to that city
-    if(!(city in this.passengers)){
-      this.passengers[city]=0
+    if (!(city in this.passengers)) {
+      this.passengers[city] = 0
     }
-    this.passengers[city]+=num
+    this.passengers[city] += num
   }
-  alightPassengersForCity(city){
-    if(!(city in this.passengers)){
+  alightPassengersForCity(city) {
+    if (!(city in this.passengers)) {
       console.error(`Error in alightPassengersForCity where city = ${city}`)
     }
     let num = this.passengers[city]
-    this.passengers[city]=0
+    this.passengers[city] = 0
     //return number of pasengers alighted
     return num
   }
-  get occupancy(){
-    return 1-this.passenger_room_available/this.passenger_capacity
+  get occupancy() {
+    return 1 - this.passenger_room_available / this.passenger_capacity
   }
-  get wagonloading(){
-    return 1-this.passenger_room_available/this.wagon_capacity
+  get wagonloading() {
+    return 1 - this.passenger_room_available / this.wagon_capacity
   }
-  saveInDB = async() => {
+  saveInDB = async () => {
     console.log(`%cSaving train running on ${this.path.number}`, 'background: #222; color: #bada55')
-    saveTrainToDB(this.path.PathIdInDB,this.passengercoaches,this.goodscoaches)
+    saveTrainToDB(this.path.PathIdInDB, this.passengercoaches, this.goodscoaches)
   }
 }
